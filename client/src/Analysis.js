4396eaa16f02b7678300dd7d2b3bbbe9d6bcad44
arupam <sahaarupam37@gmail.com>
2022-11-17 02:21:59 +0530

    fix UI and add loading msg

--- client/src/Analysis.js
+++ client/src/Analysis.js
@@ -1,22 +1,60 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
 
 function Analysis() {
   const urlParams = new URLSearchParams(window.location.search);
-  const track = urlParams.get('value') || null; 
-  const accessToken = window.localStorage.getItem('spotify_access_token');
-  const obj = eval('(' + track + ')');
+  const track = urlParams.get("value") || null;
+  const accessToken = window.localStorage.getItem("spotify_access_token");
+  const obj = eval("(" + track + ")");
+  const [showResults, setShowResults] = React.useState(false);
 
-  
+  const onClickMe = () => {
+    setShowResults(true);
+    window.location = `http://localhost:8888/createplaylist?accessToken=${accessToken}&mood=${obj["pos"]}`;
+  };
+
   return (
     <>
-      <h3>The current song you're listening to has a positivity rating of:</h3>
-      <h1>{obj["pos"]}</h1> 
-      <button 
-      className='App-link' 
-      onClick={() => window.location = `http://localhost:8888/createplaylist?accessToken=${accessToken}&mood=${obj['pos']}`}
-      >Create your custom playlist</button>
+      <div
+        className="Analysis"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <div>
+          <h3>
+            The current song you're listening to has a positivity rating of:
+          </h3>
+
+          <h1>{obj["pos"]}</h1>
+
+          <button className="create-playlist" onClick={onClickMe}>
+            Create your uplifting playlist
+          </button>
+          {showResults ? (
+            <>
+              <div className="loading-playlist">
+                <iframe
+                  src="https://giphy.com/embed/zZC2AqB84z7zFnlkbF"
+                  width="180"
+                  height="180"
+                  frameBorder="0"
+                  class="giphy-embed"
+                  allowFullScreen
+                ></iframe>
+                <p className="loading-text">
+                  Your Playlist is being created . . .{" "}
+                </p>
+              </div>
+            </>
+          ) : null}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Analysis
+export default Analysis;
--- client/src/App.js
+++ client/src/App.js
@@ -1,124 +1,166 @@
-import { accessToken, logout, getCurrentUserProfile, getCurrentTrackInfo } from './spotify';
+import {
+  accessToken,
+  logout,
+  getCurrentUserProfile,
+  getCurrentTrackInfo,
+} from "./spotify";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useEffect, useState } from 'react';
-import { catchErrors } from './utils';
-import Analysis from './Analysis';
+import { useEffect, useState } from "react";
+import { catchErrors } from "./utils";
+import Analysis from "./Analysis";
 
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpotify } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpotify } from "@fortawesome/free-brands-svg-icons";
 
-
-import './App.css';
-
+import "./App.css";
 
 function App() {
   const [token, setToken] = useState(null);
   const [track, setTrack] = useState(null);
   const [profile, setProfile] = useState(null);
-  
+
   useEffect(() => {
     setToken(accessToken);
 
-    const fetchData = async () => {    
-        const { data } = await getCurrentTrackInfo();
-        setTrack(data);
-        console.log(data);
+    const fetchData = async () => {
+      const { data } = await getCurrentTrackInfo();
+      setTrack(data);
+      console.log(data);
     };
     catchErrors(fetchData());
-    const fetchProfileData = async () => {    
+    const fetchProfileData = async () => {
       const { data } = await getCurrentUserProfile();
       setProfile(data);
       console.log(data);
-  };
-  catchErrors(fetchProfileData());
-  }, [])
-  
+    };
+    catchErrors(fetchProfileData());
+  }, []);
+
   return (
     <div className="App">
       <Helmet>
-                <meta charSet="utf-8" />
-                <link rel="canonical" href="http://example.com/example" />
-            </Helmet>
-
+        <meta charSet="utf-8" />
+        <link rel="canonical" href="http://example.com/example" />
+      </Helmet>
 
       <header className="App-header">
-        
         {!token ? (
+          <div className="landing">
+            <div>
+              <h1>
+                <FontAwesomeIcon icon={faSpotify} /> Uplift{" "}
+              </h1>
+              <p>A realtime music analyzer </p>
 
-        <div className='landing'>
-          <div>
-            <h1><FontAwesomeIcon icon={faSpotify} /> Uplift </h1>
-            <p>A realtime music analyzer </p>
-            
-            <a
-            className="App-link"
-            href="http://localhost:8888/login"
-          >
-              Login with Spotify
-            </a>
+              <a className="App-link" href="http://localhost:8888/login">
+                Login with Spotify
+              </a>
             </div>
-        </div>
-        
-        ):(
-        <>
-          <BrowserRouter>
-            <Routes>
-              <Route 
-                path="/"
-                element={
-                  <>
-                  
-                           
-                  {profile && (
+          </div>
+        ) : (
+          <>
+            <BrowserRouter>
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <>
+                      {profile && (
+                        <div className="navbar">
+                          <div>
+                            <a className="brand" href="/">
+                              <FontAwesomeIcon icon={faSpotify} /> Uplift{" "}
+                            </a>
+                          </div>
 
-                    <div className="navbar">
-                      <div>
-                        <a className="brand" href='/'><FontAwesomeIcon icon={faSpotify} /> Uplift </a>
-                      </div>
-                      
-
-                      <div className="menu">
-                        {profile.images.length && profile.images[0].url && (
-                          <img className='profile-img' src={profile.images[0].url} alt="Avatar" />
-                        )}
-                        <p className='profile-name'>{profile.display_name}</p>
-                      </div>
-                      <div>
-                      <button className='logout-btn' onClick={logout}>Log Out</button> 
-                      </div>
-                      
-                    </div>
-                    
-                    
+                          <div className="menu">
+                            {profile.images.length && profile.images[0].url && (
+                              <img
+                                className="profile-img"
+                                src={profile.images[0].url}
+                                alt="Avatar"
+                              />
+                            )}
+                            <p className="profile-name">
+                              {profile.display_name}
+                            </p>
+                          </div>
+                          <div>
+                            <button className="logout-btn" onClick={logout}>
+                              Log Out
+                            </button>
+                          </div>
+                        </div>
                       )}
-                    
-                    {track && (
-                      <div>
-                        <p>You're listening to</p>
-                        <h1>{track.item.name}</h1>
-                        <h3>by {track.item.album.artists[0].name}</h3>
-                        {/*<a href='http://localhost:8888/analyze'>Analyze</a>*/}
-                        <button className='analyze-btn' onClick={
-                          () => {window.location=`http://localhost:8888/analyze?trackName=${track.item.name}&artistName=${track.item.album.artists[0].name}`}
-                        }>Analyze</button>
-                      </div>
-                    )}
-                  </>
-                }
-              />
-              <Route 
-                path="analyze"
-                element = {
-                <>
-                {/*<h1>{track.item.name} has the following rating</h1>*/}
-                <Analysis/>
-                </>
-                }
-              />
-            </Routes>
-          </BrowserRouter>
+
+                      {(track && track.item.type === "track") ? (
+                        <div>
+                          <h3>You're listening to</h3>
+
+                        <div className="track-details">
+                          <img
+                            height="200"
+                            width="200"
+                            className="track-img"
+                            src={track.item.album.images[0].url}
+                            alt="Avatar"
+                          />
+                          
+                          <h5 className="trackName">{track.item.name}</h5>
+                          <h6 className="trackArtist">
+                            by {track.item.album.artists[0].name}
+                          </h6>
+                          
+                          </div>
+
+                          
+                          {/*<a href='http://localhost:8888/analyze'>Analyze</a>*/}
+                          <button
+                            className="analyze-btn"
+                            onClick={() => {
+                              window.location = `http://localhost:8888/analyze?trackName=${track.item.name}&artistName=${track.item.album.artists[0].name}`;
+                            }}
+                          >
+                            Analyze
+                          </button>
+                        </div>
+                      ) : (
+                        <>
+                          <p>You're not listening to anything at the moment</p>
+                          <iframe
+                            src="https://giphy.com/embed/q9EUjcUOT1aYE"
+                            width="480"
+                            height="245"
+                            frameBorder="0"
+                          ></iframe>
+                          <br></br>
+                          <button
+                            className="logout-btn"
+                            onClick={() => {
+                              window.location = `http://localhost:8888/login`;
+                            }}
+                          >
+                            play & reload
+                          </button>
+                        </>
+                      )}
+                    </>
+                  }
+                />
+                
+                <Route
+                  path="analyze"
+                  element={
+                    <>
+                      {/*<h1>{track.item.name} has the following rating</h1>*/}
+                      <Analysis />
+                    </>
+                  }
+                />
+              </Routes>
+            </BrowserRouter>
           </>
         )}
       </header>
